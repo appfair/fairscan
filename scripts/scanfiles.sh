fe793641e84d3503f7abfb959bee8d1350c50f53
appfairbot <appfairbot@appfair.net>
2022-03-01 16:47:42 +0100

    scan

---
+++ scripts/scanfiles.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Downloads the latest cask list and fetches the scanner reports
+
+DIR=files/
+
+curl -O -fsSL http://formulae.brew.sh/api/cask.json
+
+mkdir -p ${DIR}/
+
+# clear out quota exceeded errors
+# rm -fv EMPTY_ARG `jq -r 'select(.error.code == "QuotaExceededError") | input_filename' ${DIR}/*.json`
+rm -fv EMPTY_ARG `jq -r 'select(.error.code != null) | input_filename' ${DIR}/*.json`
+
+
+# walk through each element, get the checksum, and check it against VT
+for hash in `jq -r '.[].sha256' cask.json | grep -v 'no_check' | sort --sort=random`; do
+    #echo "${hash}"
+    if [ ! -s "${DIR}/${hash}.json" ]; then
+        sleep 15 # public api request quota: 4/min, 500/day
+        curl -o ${DIR}/"${hash}.json" -sSL --request GET --url "https://www.virustotal.com/api/v3/files/${shasum}" --header "x-apikey: ${VTAPIKEY}"
+
+        cat ${DIR}/"${hash}.json" | jq '.data.attributes.names'
+        #cat ${DIR}/"${hash}.json" | jq '.data.attributes.total_votes'
+
+        # backoff if we hit a QuotaExceededError error code
+        cat ${DIR}/"${hash}.json" | jq -e '.error.code == "QuotaExceededError"' && rm ${DIR}/"${hash}.json" && sleep 100
+    fi
+done
+
+# show a report of files and errors
+cat ${DIR}/*.json | jq '.error.code' | sort | uniq -c | sort -n
+
