b319d00205357293490811b861d9b1121220de13
appfairbot <appfairbot@appfair.net>
2022-03-02 23:42:19 +0100

    Scan Hashes

--- scripts/scanhashes.sh
+++ scripts/scanhashes.sh
@@ -11,7 +11,7 @@ curl -O -fsSL http://formulae.brew.sh/api/cask.json
 mkdir -p ${DIR}/
 
 # walk through each element, get the checksum, and check it against VT
-for shaurl in `jq -r '.[] | "\(.sha256)|\(.url)"' cask.json`; do
+for shaurl in `jq -r '.[] | "\(.sha256)|\(.url)"' cask.json | sort --sort=random`; do
 
     hash=`echo "${shaurl}" | cut -f 1 -d '|'`
     url=`echo "${shaurl}" | cut -f 2- -d '|'`
@@ -34,7 +34,9 @@ for shaurl in `jq -r '.[] | "\(.sha256)|\(.url)"' cask.json`; do
         cat ${DIR}/"${hash}.json" | jq -e '.error.code == "QuotaExceededError"' && rm ${DIR}/"${hash}.json" && exit 6
 
         # any URLs that are not found get a scan request
-        cat ${DIR}/"${hash}.json" | jq -e '.error.code == "NotFoundError"' && echo "Requesting scan for ${url}…" && curl -fsSL -o "${dlpath}" "${url}" && ulurl=`curl -fsSL "https://www.virustotal.com/api/v3/files/upload_url" --header "x-apikey: ${VTAPIKEY}" | jq -r '.data'` && curl --sSL --request POST --url "${ulurl}" --header "x-apikey: ${VTAPIKEY}" --header 'Accept: application/json' --header 'Content-Type: multipart/form-data' --form "file=@${dlpath}"
+        cat ${DIR}/"${hash}.json" | jq -e '.error.code == "NotFoundError"' && echo "Downloading ${url}…" && ulurl=`curl -fsSL "https://www.virustotal.com/api/v3/files/upload_url" --header "x-apikey: ${VTAPIKEY}" | jq -r '.data'` && curl -fsSL --max-filesize 200m -o "${dlpath}" "${url}" && echo "Requesting scan for ${url}…" && curl -o ${DIR}/"${hash}.json" -fsSL --request POST --url "${ulurl}" --header "x-apikey: ${VTAPIKEY}" --header 'Accept: application/json' --header 'Content-Type: multipart/form-data' --form "file=@${dlpath}"
+
+        cat ${DIR}/"${hash}.json" | jq -e '.error.code == "QuotaExceededError"' && rm ${DIR}/"${hash}.json" && exit 6
 
         scancount=$((scancount + 1))
         if [ ${scancount} -gt ${scancountmax} ]; then
@@ -50,4 +52,7 @@ cat ${DIR}/*.json | jq '.error.code' | sort | uniq -c | sort -n
 # clear out quota exceeded errors
 # rm -fv EMPTY_ARG `jq -r 'select(.error.code == "QuotaExceededError") | input_filename' ${DIR}/*.json`
 rm -fv EMPTY_ARG `jq -r 'select(.error.code != null) | input_filename' ${DIR}/*.json`
+
+# clear out urls that were queued for analysis
+rm -fv EMPTY_ARG `jq -r 'select(.data.type == "analysis") | input_filename' ${DIR}/*.json`
 
